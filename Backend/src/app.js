fdbbadad60abaf2969106e5c43794928c99ccf17
Alpha_Q09 <146162443+Omtambe99@users.noreply.github.com>
2026-08-13 13:51:12 +0530

    feat: update server configuration and improve API URL handling

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,11 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-const allowedOrigins = ["http://localhost:5173", process.env.FRONTEND_URL];
+const allowedOrigins = ["http://localhost:5173"];
+
+if (process.env.FRONTEND_URL) {
+  allowedOrigins.push(process.env.FRONTEND_URL);
+}
 
 app.use(
   cors({
@@ -29,25 +33,3 @@ app.use("/api/auth", authRouter);
 app.use("/api/interview", interviewRouter);
 
 module.exports = app;
-// const express = require("express")
-// const cookieParser = require("cookie-parser")
-// const cors = require("cors")
-
-// const app = express()
-
-// app.use(express.json())
-// app.use(cookieParser())
-// app.use(cors({
-//     origin: process.env.FRONTEND_URL,
-//     credentials: true
-// }))
-
-// /* require all the routes here */
-// const authRouter = require("./routes/auth.routes")
-// const interviewRouter = require("./routes/interview.routes")
-
-// /* using all the routes here */
-// app.use("/api/auth", authRouter)
-// app.use("/api/interview", interviewRouter)
-
-// module.exports = app
